e5ffd30a1ba30022a6e6e7788ebae1ca9a7df087
Carson Snow <anxiouswildflower97@gmail.com>
2024-02-25 21:38:56 -0500

    tutorial 1.3 output

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+let index=0;
+const counterDisplay = document.getElementById('display');
+
+function countIncrease(){
+    index ++;
+    counterDisplay.innerHTML = index;
+}
+
+function countDecrease(){
+    index --;
+    counterDisplay.innerHTML = index;
+}
+
+function countReset(){
+    index=0;
+    counterDisplay.innerHTML = index;
+}
